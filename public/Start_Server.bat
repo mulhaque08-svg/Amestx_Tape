a5bac47b2687f58c7ffd51cf679e2c6f2ee49425
mulhaque08-svg <mulhaque08@gmail.com>
2026-09-24 17:17:29 -0500

    Delete public directory

--- public/Start_Server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title TxDOT Estimator Web Server (Port 8081)
-echo =========================================================================
-echo  Starting Amestx TxDOT Estimator Local Web Server on http://localhost:8081/
-echo =========================================================================
-echo.
-start http://localhost:8081/
-powershell -ExecutionPolicy Bypass -File "%~dp0server.ps1" -port 8081
-pause
